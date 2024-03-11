533cf3fcf8aaeea1124d97cfc61ded96b3418b53
Yuliy-ch <yuliy.huang@nyonic.ai>
2024-03-11 16:57:31 +0800

    elTableInfiniteScroll

--- src/main.js
+++ src/main.js
@@ -4,11 +4,12 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import router from './router.js'
 import * as echarts from 'echarts';
+import elTableInfiniteScroll from 'el-table-infinite-scroll';
 
 
 const app = createApp(App)
 
-app.use(router).use(ElementPlus)
+app.use(router).use(ElementPlus).use(elTableInfiniteScroll)
 app.config.globalProperties.$echarts = echarts
 
 app.mount('#app')
