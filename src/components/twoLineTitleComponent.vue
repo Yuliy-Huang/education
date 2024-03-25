6a15e2581301a21e74f0f8376843524542364b0e
Yuliy-ch <yuliy.huang@nyonic.ai>
2024-03-25 15:56:17 +0800

    student

---
+++ src/components/twoLineTitleComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="two-line-title">
+    <div class="first-line">
+      <div class="first-col">left</div>
+      <div class="first-col">学校六个专业［共{{ total }}人］学员统计表</div>
+      <div class="first-col">right</div>
+    </div>
+    <div class="second-line">
+      <div class="second-col" v-for="(item, index) in majorList" :key="index">
+        {{ item.major }}［{{ item.count }}J人
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  setup() {},
+};
+</script>
+<style scoped lang="less">
+@import '@/assets/css/twoLineTitleComponentCss.less';
+</style>
--- src/views/campusStudent/indexPage.vue
+++ src/views/campusStudent/indexPage.vue
@@ -1,10 +1,90 @@
 <template>
-  <div>
-   campus student
-  </div>
+  <pageStructureComponent
+    :pageType="'home'"
+    :isSeparate="isSeparate"
+    @close2NotDim="close2NotDim"
+    @back2LastDiv="back2LastDiv"
+  >
+    <component
+      :is="currentCom"
+      :page-type="pageType"
+      :staffList="staffList"
+      :searchValue="searchValue"
+      @changeTab="changeTab"
+      :blockList="blockList"
+      :placeholder="'校区学员搜索：'"
+    />
+  </pageStructureComponent>
 </template>
 
-
 <script setup>
-</script>
+import { markRaw, ref, watch, defineAsyncComponent } from 'vue';
+import pageStructureComponent from '@/components/pageStructureComponent';
+import blocksComponent from '@/components/blocksComponent';
 
+const studentNumberPage = defineAsyncComponent(() =>
+  import('./studentNumber.vue')
+);
+const classStatisticPage = defineAsyncComponent(() =>
+  import('./studentStatistic.vue')
+);
+const fileSeePage = defineAsyncComponent(() => import('./studentFileSee.vue'));
+const studentDimissionPage = defineAsyncComponent(() =>
+  import('./studentDimission.vue')
+);
+
+const pageType = ref('home');
+const isSeparate = ref(false);
+const blockList = ref([
+  { name: '学员专业人数', pageType: 'studentNumber' },
+  { name: '学员课时统计', pageType: 'classStatistic' },
+  { name: '学员档案查看', pageType: 'fileSee' },
+  { name: '学员退学管理', pageType: 'studentDimission' },
+]);
+const staffList = ref(['梁朝伟', '刘德华']);
+const searchValue = ref('');
+
+const changeTab = v => {
+  console.log('indexPage.vue --- v : ', v);
+  pageType.value = v;
+  isSeparate.value = [].includes(v);
+};
+
+const close2NotDim = () => {
+  changeTab('home');
+};
+
+const back2LastDiv = () => {
+  switch (pageType.value) {
+    case 'studentNumber':
+    case 'classStatistic':
+    case 'fileSee':
+    case 'studentDimission':
+      changeTab('home');
+      break;
+    default:
+      changeTab('home');
+  }
+};
+
+const currentCom = ref(markRaw(blocksComponent));
+watch(pageType, () => {
+  switch (pageType.value) {
+    case 'studentNumber':
+      currentCom.value = markRaw(studentNumberPage);
+      break;
+    case 'classStatistic':
+      currentCom.value = markRaw(classStatisticPage);
+      break;
+    case 'fileSee':
+      currentCom.value = markRaw(fileSeePage);
+      break;
+    case 'studentDimission':
+      currentCom.value = markRaw(studentDimissionPage);
+      break;
+    default:
+      currentCom.value = markRaw(blocksComponent);
+  }
+});
+</script>
+<style scoped lang="less"></style>
