7bd1ab214c05d99d7abcff514fa7683390cc0124
Yuliy-ch <yuliy.huang@nyonic.ai>
2024-04-03 00:22:18 +0800

    css

--- src/views/login/registerCampus.vue
+++ src/views/login/registerCampus.vue
@@ -1,19 +1,33 @@
 <template>
   <div class="register-form">
     <div class="campus-form">
-      <div>校区注册</div>
-      <div>校区注册</div>
-      <div>校区注册</div>
-      <div>校区注册</div>
-      <div>校区注册</div>
-      <div>
-        <div>确定</div>
-        <div>取消</div>
+      <div class="campus-info">
+        <div>校区注册</div>
+        <div class="top-div">校区名称添加1</div>
+        <div class="bottom-div">校区注册</div>
+        <div class="top-div">校区名称添加2</div>
+        <div class="bottom-div">校区注册</div>
+        <div class="button-div">
+          <div class="left-arrow">
+            <img
+              :src="require(`@/assets/img/arrowLeft.png`)"
+              style="width: 24px; height: 14px"
+              alt=""
+            />
+          </div>
+          <div class="right-arrow">
+            <img
+              :src="require(`@/assets/img/arrowRight.png`)"
+              style="width: 24px; height: 14px"
+              alt=""
+            />
+          </div>
+        </div>
       </div>
     </div>
-    <div class="campus-button">
-      <div>确定</div>
-      <div>取消</div>
+    <div class="campus-button-div">
+      <el-button>确定</el-button>
+      <el-button>取消</el-button>
     </div>
   </div>
 </template>
