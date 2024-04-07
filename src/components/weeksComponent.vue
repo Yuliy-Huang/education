a11177b31b0851b817b6dc79da65e5e2a08e4c6a
Yuliy-ch <yuliy.huang@nyonic.ai>
2024-04-07 20:48:13 +0800

    queryManageDone

---
+++ src/components/weeksComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="homework-left-side">
+    <div>
+      <img
+        :src="require(`@/assets/img/qr/img.jpg`)"
+        style="width: 80px; height: 120px"
+        alt=""
+      />
+    </div>
+    <div>刘老师</div>
+    <div>钢琴教师</div>
+  </div>
+
+  <div class="homework-right-side">
+    <div class="homework-line-inside">
+      <div class="homework-cell" v-for="itm of 5" :key="itm">
+        {{ toChineseNumber(itm - 1) }} 级
+      </div>
+    </div>
+    <div class="homework-line-inside">
+      <div class="homework-cell" v-for="itm of 5" :key="itm">
+        {{ itm - 1 }} 级
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { toChineseNumber } from '@/utils/numberFormat';
+export default {
+  methods: {
+    toChineseNumber: toChineseNumber,
+  },
+  props: {
+    showLeftSide: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props) {
+    return {
+      props,
+    };
+  },
+};
+</script>
+<style scoped lang="less">
+@import '@/assets/css/weeksComponentCss.less';
+</style>
