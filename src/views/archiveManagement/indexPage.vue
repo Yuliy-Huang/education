70a7953a39dcee8855c72122cee055d3466fc6c1
Yuliy-ch <yuliy.huang@nyonic.ai>
2024-04-09 22:38:55 +0800

    indexOfArchieve

--- src/views/archiveManagement/indexPage.vue
+++ src/views/archiveManagement/indexPage.vue
@@ -11,15 +11,21 @@
       :blockList="blockList"
       @changeTab="changeTab"
       :twoBlockList="twoBlockList"
+      :searchValue="searchValue"
+      :placeholder="placeholder"
+      :staffList="staffList"
     />
   </pageStructureComponent>
 </template>
 
 <script setup>
-import { markRaw, ref, watch } from 'vue';
+import { markRaw, ref, watch, defineAsyncComponent } from 'vue';
 import pageStructureComponent from '@/components/pageStructureComponent';
 import blocksComponent from '@/components/blocksComponent';
 import twoBlocksComponent from '@/components/twoBlocksComponent';
+const cellMoreSearchComponent = defineAsyncComponent(() =>
+  import('../../components/cellMoreSearchComponent.vue')
+);
 
 const pageType = ref('home');
 const isSeparate = ref(false);
@@ -32,12 +38,18 @@ const blockList = ref([
 ]);
 
 const twoBlockList = ref([]);
+const searchValue = ref('');
+const placeholder = ref('');
+const staffList = ref(['张三［前台］', '李四毛［经理］']);
 
 const changeTab = (from, to) => {
   console.log('archiveManage --- indexPage.vue --- from : ', from);
   console.log('archiveManage --- indexPage.vue --- to : ', to);
-
-  pageType.value = to;
+  if (from === 'home' || !from) {
+    pageType.value = to;
+  } else {
+    pageType.value = from + '-' + to;
+  }
   isSeparate.value = [''].includes(to);
   switch (pageType.value) {
     case 'page1':
@@ -46,8 +58,19 @@ const changeTab = (from, to) => {
     case 'page4':
       twoBlockList.value = ['代课教师管理存栏', '教务员工管理存栏'];
       break;
+    case 'page1-teacher':
+    case 'page2-teacher':
+    case 'page3-teacher':
+    case 'page4-teacher':
+      placeholder.value = '代课教师搜索：';
+      break;
+    case 'page1-staff':
+    case 'page2-staff':
+    case 'page3-staff':
+    case 'page4-staff':
+      placeholder.value = '教务员工搜索：';
+      break;
     default:
-      console.log('changeTab --- default');
       twoBlockList.value = [];
   }
 };
@@ -57,21 +80,16 @@ const close2NotDim = () => {
 };
 
 const back2LastDiv = () => {
-  switch (pageType.value) {
-    case 'xxx':
-      changeTab('home', 'page1');
-      break;
-    case 'www':
-      changeTab('home', 'page2');
-      break;
-    case 'eeee':
-      changeTab('home', 'page3');
-      break;
-    case 'wwwwww':
-      changeTab('home', 'page4');
-      break;
-    default:
-      changeTab('', 'home');
+  if (pageType.value.startsWith('page1-')) {
+    changeTab('', 'page1');
+  } else if (pageType.value.startsWith('page2-')) {
+    changeTab('', 'page2');
+  } else if (pageType.value.startsWith('page3-')) {
+    changeTab('', 'page3');
+  } else if (pageType.value.startsWith('page4-')) {
+    changeTab('', 'page4');
+  } else {
+    changeTab('', 'home');
   }
 };
 
@@ -83,6 +101,16 @@ watch(pageType, () => {
     case 'page3':
     case 'page4':
       currentCom.value = markRaw(twoBlocksComponent);
+      break;
+    case 'page1-teacher':
+    case 'page1-staff':
+    case 'page2-teacher':
+    case 'page2-staff':
+    case 'page3-teacher':
+    case 'page3-staff':
+    case 'page4-teacher':
+    case 'page4-staff':
+      currentCom.value = markRaw(cellMoreSearchComponent);
       break;
     default:
       currentCom.value = markRaw(blocksComponent);
